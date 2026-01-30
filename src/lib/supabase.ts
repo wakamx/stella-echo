9763d03c91eb949ef03c06b4d7f9d4f18d45494c
WA <alloythf@gmail.com>
2026-01-30 13:39:14 +0900

    Initial

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
+// src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+// 'supabase' という名前で export する必要があります
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
